df5742bbd9cd709c26d7bdc130b38b05dafbc01c
vishal singh <vk788043@gmail.com>
2023-11-07 13:59:05 +0530

    some changes

--- assets/js/register.js
+++ assets/js/register.js
@@ -4,13 +4,14 @@ let form1= document.getElementsByClassName("form1")[0];
 let form2= document.getElementsByClassName("form2")[0];
 
 let show=()=>{
-    form1.style.display="block";
+    form1.style.display="flex";
     form2.style.display="none";
     h3[0].style.borderBottom="3px solid blue";
     h3[1].style.borderBottom="none";
 }
 let show1=()=>{
-    form2.style.display="block";
+    form2.style.display="flex";
+    form2.style.justifyContent="center"
     form1.style.display="none";
     h3[1].style.borderBottom="3px solid blue";
     h3[0].style.borderBottom="none";
